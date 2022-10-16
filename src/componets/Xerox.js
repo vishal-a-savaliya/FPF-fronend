b066aa585fa61cd18f18b019e56cd943ef3b4b50
Prit Manvar <pritmanvar@gmail.com>
2022-10-17 01:18:07 +0530

    solved totalPage problem and fixed alignment

--- src/componets/Xerox.js
+++ src/componets/Xerox.js
@@ -1,94 +1,140 @@
-import React from 'react'
-import { useState } from 'react'
-import { checkoutHandler } from '../view/payment';
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { checkoutHandler } from "../view/payment";
 
 function Xerox() {
-
-
-    const [counter, setCounter] = useState(1);
-    const [totalpage, setTotalPage] = useState(0);
-    const [inputPage, setInputPage] = useState([]);
+    const [counter, setCounter] = useState(1); // number of inputs
+    const [totalpage, setTotalPage] = useState(0); // number of totle pages
+    const [fileInput, setFileInput] = useState([]); // actual input tags
+    const [pages, setPages] = useState([0]); // number of pages in each input
 
     const handleplusbutton = () => {
-        setCounter(counter + 1);
-        // console.log(counter);
+        setCounter((prev) => prev + 1);
+        let newPages = [...pages];
+        newPages.push(0);
+        setPages(newPages);
+
+        // generating input from here so i can pass index to the fileHandler function
+        setFileInput(
+            Array.from(Array(counter + 1)).map((c, index) => {
+                return (
+                    <input
+                        key={index}
+                        type='file'
+                        accept='.pdf'
+                        required
+                        id='files'
+                        onChange={(e) => fileHandler(e, index)}
+                        className='w-[270px]'
+                    />
+                );
+            })
+        );
     };
 
+    // initially generate 1st input
+    useEffect(() => {
+        setFileInput(
+            Array.from(Array(1)).map((c, index) => {
+                return (
+                    <input
+                        key={index}
+                        type='file'
+                        accept='.pdf'
+                        required
+                        id='files'
+                        onChange={(e) => fileHandler(e, index)}
+                        className='w-[260px]'
+                    />
+                );
+            })
+        );
+    }, []);
 
-    const fileHander = (e) => {
+    const fileHandler = (e, key) => {
+        const input = e.target;
 
-        var input = e.target;
-        var reader = new FileReader();
-        reader.readAsBinaryString(input.files[0]);
-        reader.onloadend = function () {
-            var count = reader.result.match(/\/Type[\s]*\/Page[^s]/g).length;
-            console.log('Number of Pages:', count);
-            setTotalPage(totalpage + count)
+        if (input.files.length === 0) {
+            setPages((prevPages) => {
+                let newPages = [...prevPages];
+                newPages[key] = 0;
 
-            setInputPage()
+                setTotalPage((prevTotal) => prevTotal - prevPages[key] + 0);
+                return newPages;
+            });
+        } else {
+            const reader = new FileReader();
+            reader.readAsBinaryString(input.files[0]);
+            reader.onloadend = function () {
+                const count = reader.result.match(
+                    /\/Type[\s]*\/Page[^s]/g
+                ).length;
+
+                setPages((prevPages) => {
+                    let newPages = [...prevPages];
+                    newPages[key] = count;
+
+                    setTotalPage(
+                        (prevTotal) => prevTotal - prevPages[key] + count
+                    );
+                    return newPages;
+                });
+            };
         }
-
-        // console.log(input.files[0].name)
-    }
-
+    };
 
     return (
-
-        // <div> Xerox </div>
         <div className='flex justify-center items-center mt-20'>
-
             <div className='flex justify-center flex-col items-start shadow-lg rounded-lg w-[350px] h-[100%] pb-20 p-10'>
-
-
                 {/* title */}
 
-                <div className='px-4 pl-2'>
-                    <p className='my-2 text-lg text-primary font-medium text-left' >Xerox <span className='text-secondary'></span></p>
-                    <p className='my-2 text-lg text-primary font-medium text-left'> Rate : ₹ <span className='text-secondary text-base'>{0.85} per page</span></p>
+                <div className='pr-4 pl-2'>
+                    <p className='my-2 text-lg text-primary font-medium text-left'>
+                        Xerox <span className='text-secondary'></span>
+                    </p>
+                    <p className='my-2 text-lg text-primary font-medium text-left'>
+                        {" "}
+                        Rate : ₹{" "}
+                        <span className='text-secondary text-base'>
+                            {0.85} per page
+                        </span>
+                    </p>
                 </div>
-
 
                 {/* Inputes */}
 
-                <div className='pl-2'>
+                <div className='pl-2 w-[270px] overflow-hidden text-ellipsis'>
+                    <label className='text-gray-700 text-sm pt-6'>
+                        Upload your files here
+                    </label>
 
-                    <label className='text-gray-700 text-sm pt-6'>Upload your files here</label>
-
-                    <div>
-                        {Array.from(Array(counter)).map((c, index) => {
-                            return <input type="file" accept=".pdf" required id="files" onChange={fileHander} />;
-                        })}
-                    </div>
+                    <div>{fileInput}</div>
 
                     <button onClick={handleplusbutton}>ADD MORE FILES</button>
 
+                    <p className='my-2 text-lg text-primary font-medium text-left'>
+                        Total Pages: <span id='result'>{totalpage}</span>{" "}
+                    </p>
+                    <p className='my-2 text-lg text-primary font-medium text-left'>
+                        Total:{" "}
+                        <span id='result'>{Math.ceil(totalpage * 0.85)}</span>{" "}
+                    </p>
 
-                    {/* <input name="file" type="text" className="w-full my-2 pl-3 py-1 mb-3 text-base outline-1 border border-slate-700 rounded-sm outline-none" onChange={handleChange}
-                        value={count}
-                        placeholder='Enter File requrement'
-                        autocomplete="off"
-                    /> */}
-
-                    {/* {
-                        !number && <p className='text-red-600 py-3'>please enter valid number</p>
-                    } */}
-
-                    <p className='my-2 text-lg text-primary font-medium text-left'>Total Pages: <span id='result'>{totalpage}</span> </p>
-                    <p className='my-2 text-lg text-primary font-medium text-left'>Total: <span id='result'>{Math.trunc(totalpage * 0.85)}</span> </p>
-
-                    <p className='bg-primary py-2  px-6 text-white rounded-md mt-4 text-center hover:cursor-pointer' onClick={
-                        () => {
-                            const price = parseInt(0.85 * totalpage);
-                            // console.log(typeof (price))
+                    <p
+                        className='bg-primary py-2  px-6 text-white rounded-md mt-4 mx-auto text-center hover:cursor-pointer'
+                        onClick={() => {
+                            const price = Math.ceil(0.85 * totalpage);
+                            // console.log(typeof price);
                             const pa = Math.trunc(price);
-                            !isNaN(pa) && pa > 0 && checkoutHandler(pa)
-                        }}>Order</p>
-
+                            !isNaN(pa) && pa > 0 && checkoutHandler(pa);
+                        }}>
+                        Order
+                    </p>
                 </div>
-
             </div>
         </div>
-    )
+    );
 }
 
-export default Xerox
+export default Xerox;
